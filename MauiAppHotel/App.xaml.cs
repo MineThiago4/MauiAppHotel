07bc8473fcb38f7c8cdd867c7253deee0dc8ae92
Mine <Mine@UMPCAE>
2025-05-18 15:39:29 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppHotel/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿
+namespace MauiAppHotel
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            var window = base.CreateWindow(activationState);
+
+            Window.Width = 400;
+            Window.Height = 600;
+
+            return window;
+        }
+
+
+    }
+}
